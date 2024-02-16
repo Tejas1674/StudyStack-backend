657d526926fef6b4492d780f089ed7b50b7df173
Pradip Sable <sablepradipsubhash@gmail.com>
2024-02-16 15:27:39 +0530

    added .env, readme file and changed some code style

--- routes/Cpproute.js
+++ routes/Cpproute.js
@@ -3,50 +3,50 @@ const Cpp = require("../model/Cpp.js");
 const { getcpp } = require("../Controller/CppContoller.js");
 
 const router = Router();
-//for all cpp leacture
+//for all cpp lecture
 router.get("/", getcpp);
 
-//for save new leacture
+//for save new lecture
 router.post("/addcpp", async (req, res) => {
-  try {
-    const { day } = req.body;
-    const newdata = await Cpp.findOne({ day });
-    if (!newdata) {
-      const data = req.body;
-      const newcpp = new Cpp(data);
-      const responce = await newcpp.save();
-      res.status(200).json(responce);
-    } else {
-      res.status(400).json("This Day is already exist!");
-    }
-  } catch (error) {
-    console.log(err);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+	try {
+		const { day } = req.body;
+		const newdata = await Cpp.findOne({ day });
+		if (!newdata) {
+			const data = req.body;
+			const newcpp = new Cpp(data);
+			const responce = await newcpp.save();
+			res.status(200).json(responce);
+		} else {
+			res.status(400).json("This Day is already exist!");
+		}
+	} catch (error) {
+		console.log(err);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 });
 
 //for update  the lecture by day
 router.put("/cpp/update/:day", async (req, res) => {
-  try {
-    const updatedCpp = await Cpp.findOneAndUpdate(
-      { day: req.params.day },
-      {
-        $set: {
-          day: req.body.day,
-          time: req.body.time,
-          Description: req.body.Description,
-          videoLink: req.body.videoLink,
-          title: req.body.title,
-        },
-      },
-      { new: true }
-    );
+	try {
+		const updatedCpp = await Cpp.findOneAndUpdate(
+			{ day: req.params.day },
+			{
+				$set: {
+					day: req.body.day,
+					time: req.body.time,
+					Description: req.body.Description,
+					videoLink: req.body.videoLink,
+					title: req.body.title,
+				},
+			},
+			{ new: true }
+		);
 
-    res.json(updatedCpp);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+		res.json(updatedCpp);
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
 });
-//for  delete the leacture by id
+//for  delete the lecture by id
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,26 +1,37 @@
 // server.js
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const userroutes = require("./routes/UserRoute.js");
-const cpproutes= require("./routes/Cpproute.js")
+const cpproutes = require("./routes/Cpproute.js");
 const app = express();
 const cors = require("cors");
+
+// app Middlewares
 app.use(express.json());
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
-const URL =
-  "mongodb+srv://Tejas_2001:Tejas_2001@cluster0.yh6oemv.mongodb.net/LMS?retryWrites=true&w=majority";
-mongoose
-  .connect(URL)
-  .then(() => console.log(`Connected to mongob..`))
-  .catch((err) => console.log(err));
-
-  //user routes
+// All main routes
+//main routes
+app.get("/", (req, res) => {
+	// throw new Error("Fake Error generated for testing purpose");
+	res.send("Hello Node API Learning");
+});
+//user routes
 app.use("/api/v1/user", userroutes);
 
 //cpp routes
-app.use("/api/v1/cpp",cpproutes)
-const PORT = 8000;
-app.listen(PORT, () => {
-  console.log(`Server Run on ${PORT}`);
-});
+app.use("/api/v1/cpp", cpproutes);
+
+const MONGO_URL = process.env.MONGO_URL;
+const PORT = process.env.PORT_NUM || 4000;
+mongoose
+	.connect(MONGO_URL)
+	.then(() => {
+		app.listen(PORT, () => {
+			console.log(`Server Run on ${PORT}`);
+		});
+		console.log(`Connected to mongob..`);
+	})
+	.catch((err) => console.log(err));
