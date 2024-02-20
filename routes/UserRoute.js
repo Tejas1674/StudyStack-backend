37f459f097f3e8300e0893d4d16f17ac88904a81
Tejas1674 <tupinjarkar@gmail.com>
2024-02-20 23:27:42 +0530

    userroute update

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -67,7 +67,7 @@ router.post("/login", async (req, res) => {
 
     const token = generateToKen({ user });
 
-    res.status(200).json({ token: token });
+    res.status(200).json({ user:user, token: token });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
